32ea3bf1301927645e9674508f05e91ee23d7f00
Pieter De Witte <pieter@straffekoffie.com>
2018-10-06 14:53:13 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import { Link, Router } from 'react-static'
+//
+import Routes from 'react-static-routes'
+import './app.scss'
+
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <Link exact to="/">
+          Home
+        </Link>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+      </nav>
+      <div className="content">
+        <Routes />
+      </div>
+    </div>
+  </Router>
+)
+
+export default hot(module)(App)
