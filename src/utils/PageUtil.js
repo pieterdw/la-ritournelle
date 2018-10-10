1d34c6ca00bc904e239d1f0649da430c68470fc3
Pieter De Witte <pieter@straffekoffie.com>
2018-10-10 21:03:37 +0200

    Small stuff

--- src/utils/PageUtil.js
+++ src/utils/PageUtil.js
@@ -3,18 +3,21 @@ export class PageUtil {
     const result = [];
     raw.forEach(page => {
       result.push({
+        slug: page.slug,
         path: '/' + page.slug,
         locale: 'nl',
         title: page.title,
         content: page.content
       });
       result.push({
+        slug: page.slug,
         path: '/en/' + page.slug,
         locale: 'en',
         title: page.title_en || page.title,
         content: page.content_en || page.content
       });
       result.push({
+        slug: page.slug,
         path: '/fr/' + page.slug,
         locale: 'fr',
         title: page.title_fr || page.title,
