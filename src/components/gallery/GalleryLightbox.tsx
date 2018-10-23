4aa67b77f1130605c387c5fe97f7bf592c51f51a
Pieter De Witte <pieter@straffekoffie.com>
2018-10-23 19:45:08 +0200

    Different lightbox

--- src/components/gallery/GalleryLightbox.tsx
+++ src/components/gallery/GalleryLightbox.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Lightbox from 'react-image-lightbox';
-import 'react-image-lightbox/style.css';
+import Lightbox from 'react-images';
 import { GalleryImage } from 'scripts/models/Gallery';
 
 export interface GalleryLightboxProps {
@@ -14,21 +13,27 @@ export class GalleryLightbox extends React.Component<GalleryLightboxProps, {}> {
 
   public render() {
     const { images, currentImagePath } = this.props;
-    if (!currentImagePath) {
-      return null;
-    }
+    // if (!currentImagePath) {
+    //   return null;
+    // }
     const index = images.findIndex(img => img.fullSizePath === currentImagePath);
     const nextImagePath = images[(index + 1) % images.length].fullSizePath;
     const previousImagePath = images[(index + images.length - 1) % images.length].fullSizePath;
     return (
       <Lightbox
-        mainSrc={currentImagePath}
-        nextSrc={nextImagePath}
-        prevSrc={previousImagePath}
-        enableZoom={false}
-        onCloseRequest={this.handleSelect(null)}
-        onMovePrevRequest={this.handleSelect(previousImagePath)}
-        onMoveNextRequest={this.handleSelect(nextImagePath)}
+        // mainSrc={currentImagePath}
+        // nextSrc={nextImagePath}
+        // prevSrc={previousImagePath}
+        // enableZoom={false}
+        // onCloseRequest={this.handleSelect(null)}
+        // onMovePrevRequest={this.handleSelect(previousImagePath)}
+        // onMoveNextRequest={this.handleSelect(nextImagePath)}
+        images={images.map(img => ({ src: img.fullSizePath }))}
+        currentImage={index}
+        isOpen={!!currentImagePath}
+        onClickPrev={this.handleSelect(previousImagePath)}
+        onClickNext={this.handleSelect(nextImagePath)}
+        onClose={this.handleSelect(null)}
       />
     );
   }
