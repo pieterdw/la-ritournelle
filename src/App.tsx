b0802aab8f6d08b64a69f49a952d46eac27c50a5
Pieter De Witte <pieter@straffekoffie.com>
2018-10-29 11:31:00 +0100

    Poly filling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import ES6Promise from 'es6-promise';
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import { Router } from 'react-static';
 import Routes from 'react-static-routes';
 import './css/App.scss';
-
-ES6Promise.polyfill();
 
 if (process.env.NODE_ENV !== 'production') {
   const WOW = require('wowjs');
