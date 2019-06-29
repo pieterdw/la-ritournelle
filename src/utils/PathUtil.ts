a8f00c23e5cb738bf85f8fde3b19a6fe727c4ba6
Pieter De Witte <pieter@straffekoffie.com>
2019-06-29 14:17:47 +0200

    Small path fix

--- src/utils/PathUtil.ts
+++ src/utils/PathUtil.ts
@@ -3,7 +3,7 @@ import { Api } from '../../scripts/Api';
 export class PathUtil {
   public static getPathInLocale = (locale: string, id: string, slug: string) => {
     if (id === 'homepage') {
-      return locale === 'nl' ? '/' : '/' + locale;
+      return locale === 'nl' ? '/' : '/' + locale + '/';
     }
     return `/${locale}/${slug}`;
   };
